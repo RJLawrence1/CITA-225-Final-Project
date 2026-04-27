cddae919327fed8b02737ff85b6d20118528263e
RJLawrence1 <lawren610@morrisville.edu>
2026-04-27 12:21:42 -0400

    Added book class

--- CITA225FinalProject.py
+++ CITA225FinalProject.py
@@ -1,6 +1,14 @@
-import Stack
-import Queue
-import LinkedList
+from LinkedList import LinkedList
+from Queue import Queue
+from Stack import Stack
+
+class book():
+    def __init__ (self, isbn, title, author, price, quantity):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.price = price
+        self.quantity = quantity
 
 def main():
     print("Test")
